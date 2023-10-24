1548bae3d3a7ab16925f0e72dc53000b15d748ad
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-10-23 22:46:45 -0500

    completed function 4

--- functions_basic_2.py
+++ functions_basic_2.py
@@ -27,7 +27,24 @@ Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length
 3 - Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 Example: print_and_return([1,2]) should print 1 and return 2
 """
-def print_and_return(list):
-    print(list[0])
-    return list[1]
-print(print_and_return([1,2]))
+# def print_and_return(list):
+#     print(list[0])
+#     return list[1]
+# print(print_and_return([1,2]))
+
+"""
+4 - Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
+Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
+Example: values_greater_than_second([3]) should return False
+"""
+
+def values_greater_than_second(list):
+    if len(list) < 2:
+        return False
+    new_list = []
+    for i in range(0, len(list)):
+        if list[i] > list[1]:
+            new_list.append(list[i])
+    print(len(new_list))
+    return new_list
+values_greater_than_second([4])
